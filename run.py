3dd75ff797f40fecb96f454615ee2d211763e59a
Aftab <aftab.h.1993@gmail.com>
2020-12-03 05:54:20 +0000

    Added WSGI Runner

---
+++ run.py
@@ -0,0 +1,11 @@
+from app import create_app
+import logging
+
+app = create_app()
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
+if __name__ == '__main__':
+	app.run(debug=True)
